0b4b7dee934d9e8be7d0e3b47d80e8a365292ae5
Pedro Gonzalez <plgonzalezrx8@users.noreply.github.com>
2019-05-07 23:38:55 -0400

    Fixed the ability to obtain the finished image from the server.
    Working as intended.

--- PhotoColorizer/MainWindow.xaml.cs
+++ PhotoColorizer/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Shapes;
 using Algorithmia;
 using Microsoft.Win32;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace PhotoColorizer {
     /// <summary>
@@ -24,7 +25,10 @@ namespace PhotoColorizer {
     /// </summary>
     public partial class MainWindow : Window {
 
-        string imageToUploadFilename = null;
+        static string imageToUploadFilePath = null;
+        static string imageToUploadFileName = null;
+        static string downloadedImage = null;
+
 
 
         public MainWindow() {
@@ -43,14 +47,83 @@ namespace PhotoColorizer {
         public static void PicColorize() {
             JObject APIkey = JObject.Parse(File.ReadAllText(@"C:\Users\plgon\source\repos\PhotoColorizer\PhotoColorizer\APIkey.json"));
 
+
+
+
             var input = "{"
-                + "  \"image\": \"data://deeplearning/example_data/lincoln.jpg\"" // this should be changed to the image being loaded
+                + "  \"image\": \"data://plgonzalezrx8/PhotoColorizer/" + imageToUploadFileName + "\"" // this should be changed to the image being loaded
                 + "}";
             var client = new Client((string)APIkey["APIkey"]); //change the API for it to be loaded from a JSON file not exposed to Github
+            var nlp_directory = client.dir("data://plgonzalezrx8/PhotoColorizer");
+            var local_file_path = imageToUploadFilePath;
+            var destination = nlp_directory.file(imageToUploadFileName);
             var algorithm = client.algo("deeplearning/ColorfulImageColorization/1.1.13");
+
+            if (!destination.exists()) {
+                destination.put(File.OpenRead(local_file_path));
+            }
+            Console.WriteLine("After");
             algorithm.setOptions(timeout: 300); // optional
             var response = algorithm.pipeJson<object>(input);
+            dynamic outputFile = JObject.Parse(response.result.ToString());
+
+            dynamic output = outputFile.output;
+
+         
+
+            Console.WriteLine("Test");
+            Console.WriteLine(output);
+
             Console.WriteLine(response.result);
+
+            
+
+            string uploadedFileDir = output;
+
+            if (client.file(uploadedFileDir).exists()) {
+                var localFile = client.file(uploadedFileDir).getFile();
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Title = "Save the colorized image";
+                saveFileDialog1.Filter = "PNG Image|*.png";
+                saveFileDialog1.ShowDialog();
+                byte[] b = null;
+                if (saveFileDialog1.FileName != "") {
+                    using (FileStream f = localFile) {
+                        b = new byte[f.Length];
+                        f.Read(b, 0, b.Length);
+
+                    }
+
+
+
+
+
+                    using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.CreateNew))
+                    using (StreamWriter sw = new StreamWriter(s)) {
+                        s.Write(b, 0, b.Length);
+
+                        
+                    }
+
+
+                        
+
+
+
+
+                }
+
+
+
+
+
+            } else {
+                Console.WriteLine("The file does not exist");
+            }
+
+
+
+
 
         }
 
@@ -83,16 +156,21 @@ namespace PhotoColorizer {
 
             if (openDlg.FileName != "") {
                 SelectedFile.Text = openDlg.FileName;
-                imageToUploadFilename = openDlg.FileName;
+                imageToUploadFilePath = openDlg.FileName;
+                imageToUploadFileName = openDlg.SafeFileName;
 
+                
             } else {
                 SelectedFile.Text = "No file selected";
-                imageToUploadFilename = null;
+                imageToUploadFilePath = null;
+                imageToUploadFileName = null;
             }
 
         }
 
         private void Colorize_Click(object sender, RoutedEventArgs e) {
+
+            PicColorize();
 
         }
     }
